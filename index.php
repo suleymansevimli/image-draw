3355b66046c51e4b1359ee6d7ada6ea51ea0d80e
suleymansevimli <suleyman.sevimli1@gmail.com>
2018-07-24 23:23:32 +0300

    imagecrate tanımlanması

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
     // php kodlarımızı Yazıyoruz
     //imagecreate(); fonksiyonumuzu kullanacağız..
+
+
+    // bu alanda bir resim bulunacağını ve türünü bildiriyoruz //-> header();
+    header("Content-type: image/png");
+    // imagecreate fonksiyonu iki parametre alır ve ilk parametre genişlik ikinci parametre ise yükseklik değeridir.
+    $image = imagecreate(640,480);
  ?>
